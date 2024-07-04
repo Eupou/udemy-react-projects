2c697c6c136f1f1c7617f6a8b93ff1fc49d01cac
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2024-07-04 07:51:05 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar"
+import View from "./components/View"
+
+function App() {
+  return (
+    <>
+      <main className="h-screen flex">
+        <Sidebar />
+        <View />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <aside className="h-full w-1/3 bg-stone-900">oii</aside>
+}
